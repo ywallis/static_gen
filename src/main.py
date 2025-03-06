744ffc772318a303ab5e4fbaaf4d46dc191c9d97
ywallis <yann.wallis@gmail.com>
2025-03-06 08:46:02 +0800

    added html node

--- src/main.py
+++ src/main.py
@@ -1,8 +1,22 @@
 from textnode import TextNode, TextType
+from htmlnode import HTMLNode
+
 
 def main():
     test = TextNode("test", TextType.NORMAL, "www.cm")
     print(test)
+    html = HTMLNode(
+        "p",
+        "test_text",
+        None,
+        {
+            "href": "https://www.google.com",
+            "target": "_blank",
+        },
+    )
+    print(html)
 
-if __name__ == '__main__':
+    print(html.props_to_html())
+
+if __name__ == "__main__":
     main()
---
+++ src/test_htmlnode.py
@@ -0,0 +1,66 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_eq(self):
+        node = HTMLNode(
+            "p",
+            "test_text",
+            None,
+            {
+                "href": "https://www.google.com",
+                "target": "_blank",
+            },
+        )
+        node2 = HTMLNode(
+            "p",
+            "test_text",
+            None,
+            {
+                "href": "https://www.google.com",
+                "target": "_blank",
+            },
+        )
+        self.assertEqual(node, node2)
+
+    def test_not_eq(self):
+        node = HTMLNode(
+            "p",
+            "test_text",
+            None,
+            {
+                "href": "https://www.google.com",
+                "target": "_blank",
+            },
+        )
+        node2 = HTMLNode(
+            "p",
+            "test_text2",
+            None,
+            {
+                "href": "https://www.google.com",
+                "target": "_blank",
+            },
+        )
+        self.assertNotEqual(node, node2)
+
+    def test_props_to_html(self):
+        node = HTMLNode(
+            "p",
+            "test_text",
+            None,
+            {
+                "href": "https://www.google.com",
+                "target": "_blank",
+            },
+        )
+
+        self.assertEqual(
+            node.props_to_html(), 'href="https://www.google.com" target="_blank"'
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
